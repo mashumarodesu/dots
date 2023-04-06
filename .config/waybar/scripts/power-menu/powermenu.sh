11e58896631317a2ef75787b3ea631164a5d2b2b
mashumarodesu <phamthanhngan2012@gmail.com>
2023-04-06 11:17:24 +0700

    minor changes

---
+++ .config/waybar/scripts/power-menu/powermenu.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+
+# Current Theme
+dir="~/.config/waybar/scripts/power-menu/"
+theme='style'
+
+# Options
+shutdown='󰐥'
+reboot='󰑐'
+lock='󰍁'
+suspend='󰏦'
+logout=''
+yes=''
+no=''
+
+# Rofi CMD
+rofi_cmd() {
+	rofi -dmenu \
+		-theme ${dir}/${theme}.rasi
+}
+
+# Confirmation CMD
+confirm_cmd() {
+	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 200px;}' \
+		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
+		-theme-str 'listview {columns: 2; lines: 1;}' \
+		-theme-str 'element-text {horizontal-align: 0.5;}' \
+		-theme-str 'textbox {horizontal-align: 0.5; font: "JetBrains Mono Nerd Font Bold 12";}' \
+		-dmenu \
+		-p 'Confirmation' \
+		-mesg 'Are you sure?' \
+		-theme ${dir}/${theme}.rasi
+}
+
+# Ask for confirmation
+confirm_exit() {
+	echo -e "$yes\n$no" | confirm_cmd
+}
+
+# Pass variables to rofi dmenu
+run_rofi() {
+	echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+}
+
+# Execute Command
+run_cmd() {
+	selected="$(confirm_exit)"
+	if [[ "$selected" == "$yes" ]]; then
+		if [[ $1 == '--shutdown' ]]; then
+			systemctl poweroff
+		elif [[ $1 == '--reboot' ]]; then
+			systemctl reboot
+		elif [[ $1 == '--suspend' ]]; then
+			mpc -q pause
+			amixer set Master mute
+			systemctl suspend
+		elif [[ $1 == '--logout' ]]; then
+			loginctl terminate-user $USER
+		fi
+	else
+		exit 0
+	fi
+}
+
+# Actions
+chosen="$(run_rofi)"
+case ${chosen} in
+    $shutdown)
+		run_cmd --shutdown
+        ;;
+    $reboot)
+		run_cmd --reboot
+        ;;
+    $lock)
+		if [[ -x '/usr/bin/betterlockscreen' ]]; then
+			betterlockscreen -l
+		elif [[ -x '/usr/bin/i3lock' ]]; then
+			i3lock
+		fi
+        ;;
+    $suspend)
+		run_cmd --suspend
+        ;;
+    $logout)
+		run_cmd --logout
+        ;;
+esac
+
